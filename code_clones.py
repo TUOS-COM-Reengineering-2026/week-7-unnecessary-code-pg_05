8738087ad01b701fb106283af8a3dccb35ed1153
yongjiang liu <yongjiangliu129@gmail.com>
2026-03-24 14:07:11 +0000

    Implement Jaccard similarity, dot plot visualisation, remove dead code

--- code_clones.py
+++ code_clones.py
@@ -26,57 +26,33 @@ def compute_jaccard_similarity(a: str, b: str) -> float:
     a_content = read_file(a).splitlines()
     b_content = read_file(b).splitlines()
 
-    # TODO: Implement the rest of this function
-
-    return 0
+    set_a = set(a_content)
+    set_b = set(b_content)
+    intersection = set_a & set_b
+    union = set_a | set_b
+    if not union:
+        return 1.0
+    return len(intersection) / len(union)
 
 def visualise_dot_plot(a: str, b: str) -> str:
     a_content = read_file(a).splitlines()
     b_content = read_file(b).splitlines()
 
-    plot = '-' * 80 + '\n'
+    sep = '-' * 80
+    plot = sep + '\n'
     for i in range(len(a_content)):
         plot += f'x{i}: {a_content[i]}\n'
-    plot += '-' * 80 + '\n'
+    plot += sep + '\n'
     for j in range(len(b_content)):
         plot += f'y{j}: {b_content[j]}\n'
-    plot += '-' * 80 + '\n'
+    plot += sep + '\n'
 
-    # TODO: Implement the rest to return the dot plot for the two given programs (a: x-axis, b: y-axis)
+    n_x = len(a_content)
+    header_cells = [f'x{i}' for i in range(n_x)]
+    plot += '\t' + '\t'.join(header_cells) + '\t\n'
+    for j in range(len(b_content)):
+        row_cells = ['*' if a_content[i] == b_content[j] else ' ' for i in range(n_x)]
+        plot += f'y{j}\t' + '\t'.join(row_cells) + '\t\n'
+    plot += sep
 
-    # Below is a sample output:
-    '''
-    --------------------------------------------------------------------------------
-    x0: def calculate_total(values: list):
-    x1:     total = 0
-    x2:     for value in values:
-    x3:         if value < 0:
-    x4:             continue
-    x5:         total += value
-    x6:     return total
-    --------------------------------------------------------------------------------
-    y0: def calculate_total(values: list):
-    y1:     total = 0
-    y2:     for value in values:
-    y3:         if value < 0:
-    y4:             print("Negative value!")
-    y5:             continue
-    y6:         total += value
-    y7:     return total
-    --------------------------------------------------------------------------------
-        x0	x1	x2	x3	x4	x5	x6	
-    y0	*	 	 	 	 	 	 	
-    y1	 	*	 	 	 	 	 	
-    y2	 	 	*	 	 	 	 	
-    y3	 	 	 	*	 	 	 	
-    y4	 	 	 	 	 	 	 	
-    y5	 	 	 	 	*	 	 	
-    y6	 	 	 	 	 	*	 	
-    y7	 	 	 	 	 	 	*	
-    --------------------------------------------------------------------------------
-    '''
-
-    plot += '\t'
-    plot += '-' * 80
-
-    return plot.strip()
+    return plot
